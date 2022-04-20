37783d1f269c908c4939b5e7471454c9e892c70f
Donci31 <szigetidonat@gmail.com>
2022-04-20 13:42:36 +0200

    Snake Yaml and pom.xml update

---
+++ src/main/java/Prototype.java
@@ -0,0 +1,9 @@
+import org.yaml.snakeyaml.Yaml;
+
+public class Prototype {
+    public static void main(String[] args) {
+        Virologist v = new Virologist();
+
+        Yaml yaml = new Yaml();
+    }
+}
