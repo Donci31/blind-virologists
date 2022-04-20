0294ad229e41cd4f2798717daf38f10b30c3b0da
Donci31 <82709410+Donci31@users.noreply.github.com>
2022-04-20 18:59:35 +0200

    Delete Prototype.java

--- src/main/java/Prototype.java
+++
@@ -1,9 +0,0 @@
-import org.yaml.snakeyaml.Yaml;
-
-public class Prototype {
-    public static void main(String[] args) {
-        Virologist v = new Virologist();
-
-        Yaml yaml = new Yaml();
-    }
-}
