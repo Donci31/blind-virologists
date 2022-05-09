78bb718d86cb36868d17a4ec55d169073ebf1c02
szilaard <szilaard11@gmail.com>
2022-05-09 15:41:39 +0200

    Mapgenerate helyeket beállítja, de még a távolságot meg kell adni mert eddig kb csak elforgatja

--- src/main/java/model/Map.java
+++ src/main/java/model/Map.java
@@ -1,7 +1,11 @@
 package model;
 
 import model.fields.Field;
-
+import model.fields.Laboratory;
+import model.fields.Shelter;
+import model.fields.Warehouse;
+import view.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 /**
@@ -18,12 +22,41 @@ public class Map {
 	public void generateMap() {
 		//TODO
 		int k=20;
+		int labor=0;
+		ArrayList<FieldView> fviewlist=new ArrayList<FieldView>();
 		for(int h=0;h<k;h++) {
+			double n=Math.random();
+			if(n<0.2){
+				fields.add(new Field());
+				fviewlist.add(new FieldView(new Point(0,0),fields.get(h)));
+			}
+			else if(n<0.53){
+				fields.add(new Laboratory());
+				fviewlist.add(new LaboratoryView(new Point(0,0), (Laboratory) fields.get(h)));
+				labor+=1;
+			}   //itt majd lehet kell hogy az összes kódot lerakja ha az kritérium
+			else if(n<0.6){
+				fields.add(new Shelter());
+				fviewlist.add(new ShelterView(new Point(0,0), (Shelter) fields.get(h)));
+			}
+			else{
+				fields.add(new Warehouse());
+				fviewlist.add(new WarehouseView(new Point(0,0), (Warehouse) fields.get(h)));
+			}
 			fields.add(new Field());
+
+		}
+		if(labor==0){				//Ha nincs benne labor akkor tesz bele egyet
+			int hany=(int)Math.random()*k;
+			fields.set(hany,new Laboratory());
+			fviewlist.set(hany,new LaboratoryView(new Point(0,0), (Laboratory) fields.get(hany)));
+		}
+		for(Field i:fields){
 			for (int cs = 0; cs < 6; cs++) {
-				fields.get(h).getNeighbors().add(null);
+				i.getNeighbors().add(null);
 			}
 		}
+
 		int i=1;
 		int currfield=0;
 		while(i<k){   //i=1-től mert a base is field
@@ -35,6 +68,8 @@ public class Map {
 					if (prevfield.getNeighbors().get(j) == null) {
 						prevfield.getNeighbors().set(j, fields.get(i));//hozzáadni a j-edik szomszédhoz órajárásával megegyező irány
 						prevfield.getNeighbors().get(j).getNeighbors().set((j +3) % 6, prevfield);//kölcsönösen hozzáadja
+						Point prev=fviewlist.get(fields.indexOf(prevfield)).Getpos();
+						fviewlist.get(i).Getpos().setLocation(prev.x+Math.sin(-i*Math.PI/6.0),prev.y+Math.cos(-i*Math.PI/6.0));//TODO kipróbálni hogy jó-e a helye
 						i++;
 						if (prevfield.getNeighbors().get((j - 1+6) % 6) != null) { //balra levő mezővel összeköti, ha van
 							int g = (j - 1+6) % 6;
@@ -50,6 +85,9 @@ public class Map {
 
 				}
 			}
+		}
+		for(Drawable d:fviewlist){
+			Game.addDrawable(d);
 		}
 	}
 
